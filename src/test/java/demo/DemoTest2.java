3c326d58591aeb61c092c42096374e87680209ac
user <user@DESKTOP-F7RQNE7>
2022-11-24 19:30:57 +0530

    demo test 2

---
+++ src/test/java/demo/DemoTest2.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 
+{
+	@Test
+	public void demoTest()
+	{
+		System.out.println("Hello this is WASM6 Framework...");
+	}
+
+}
