3401eb6bddc66949348117db572616a876e59ef5
user <user@DESKTOP-F7RQNE7>
2022-11-24 12:24:13 +0530

    Commit 1 - DemoTest

---
+++ src/test/java/demo/DemoTest.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void demo()
+	{
+		
+		System.out.println("Hi this is WASM6 Framework");
+
+	}
+
+}
